4f0016737b85cc380676b663147cf42ca3590f3a
vladcuk <102167622+vladcuk@users.noreply.github.com>
2022-03-22 19:58:04 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+from aiogram.utils import executor
+
+from loader import dp
+from utils.notify_admins import on_startup_notify
+import middlewares, filters, handlers
+
+
+async def on_startup(dispatcher):
+    await on_startup_notify(dispatcher)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
